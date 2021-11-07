4659cb2e00c9824e7d3df9a25c79827896026dbf
fahrizal4111 <41119029@mahasiswa.undira.ac.id>
2021-11-07 23:29:39 +0700

    first commit

---
+++ src/app/hlmabout/hlmabout.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hlmabout',
+  templateUrl: './hlmabout.page.html',
+  styleUrls: ['./hlmabout.page.scss'],
+})
+export class HlmaboutPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
